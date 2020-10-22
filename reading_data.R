7417a3ea7739fcaf2a2a82bf83c0ebbff10a5156
willdebras <wtb3na@virginia.edu>
2020-10-22 12:52:57 -0500

    simple map output

--- reading_data.R
+++ reading_data.R
@@ -1,2 +1,17 @@
 #reading shapes
 
+library(geojsonio)
+library(broom)
+library(sp)
+
+spdf <- geojsonio::geojson_read("elections_features.geojson",  what = "sp")
+
+
+spdf_fortified <- broom::tidy(spdf) %>% dplyr::filter(!id==1)
+
+library(ggplot2)
+
+ggplot() +
+  geom_polygon(data = spdf_fortified, aes( x = long, y = lat, group = group), fill = NA, colour = "#00abff") +
+  theme_void() +
+  coord_map()
